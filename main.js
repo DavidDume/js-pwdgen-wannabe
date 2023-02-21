04adedec02b9fe2743daa7e62b519458906aa3c5
David Dume <dumedavid11@gmail.com>
2023-02-21 16:08:14 +0100

    ho aggiunto un po di decorazione

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const nome = prompt("Inserisci nome:");
 const cognome = prompt("Inserici cognome:");
 const colore = prompt("Inserisci il tuo colore preferito:");
+const anno = "2023";
 
 const passwordHtml = document.getElementById('password');
-passwordHtml.innerHTML = nome + cognome + colore;
+passwordHtml.innerHTML = nome + cognome + colore + anno;
